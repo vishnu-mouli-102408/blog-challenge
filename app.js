2a530b2a5506de6eb8c0007f9fcc4c3e0d8cc5d8
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-22 18:23:19 +0530

    secured mongodb url using environment variables

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+require("dotenv").config();
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/BlogDB");
+mongoose.connect(process.env.SERVER_URL);
 
 const postSchema = {
   title: String,
